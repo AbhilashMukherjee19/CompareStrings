565ef356ea0dc56e6a3024d816f3a7169249ffa0
AbhilashMukherjee19 <abhilashtells@gmail.com>
2022-03-02 09:39:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first string : ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter the second string : ");
+            string str2 = Console.ReadLine();
+            CompareStrings(str1, str2);
+        }
+
+        private static void CompareStrings(string str1, string str2)
+        {
+            bool flag = Equals(str1, str2);
+            if (flag)
+            {
+                Console.WriteLine("Both strings are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both strings are not equal");
+            }
+        }
+    }
+}
